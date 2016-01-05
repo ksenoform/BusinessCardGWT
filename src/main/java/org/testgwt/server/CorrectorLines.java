d01fdad6b7c111dc522cf39497f8143bbbd2870c
Robert <rszczygielski@luxoft.com>
2016-01-05 15:24:50 +0100

    Length of the lines with stars is matched to the longest word in sentence + 4 characters

---
+++ src/main/java/org/testgwt/server/CorrectorLines.java
@@ -0,0 +1,10 @@
+package org.testgwt.server;
+
+import java.util.List;
+
+/**
+ * Created by RSzczygielski on 2016-01-05.
+ */
+public interface CorrectorLines {
+    void correctLinesWithStars(List<String> listToCorrect);
+}
--- src/main/java/org/testgwt/server/TextManipulatorImpl.java
+++ src/main/java/org/testgwt/server/TextManipulatorImpl.java
@@ -4,26 +4,42 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.testgwt.client.services.TextManipulator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by RSzczygielski on 2016-01-04.
  */
-public class TextManipulatorImpl extends RemoteServiceServlet implements TextManipulator{
+public class TextManipulatorImpl extends RemoteServiceServlet implements TextManipulator, CorrectorLines{
+    private Integer longestWordInSentence;
 
     @Override
     public List<String> afterManipulate(String text) {
         String[] afterSplit = text.split("\\s");
         List<String> sList = new ArrayList<String>();
+        longestWordInSentence = 0;
 
         sList.add("*");
 
         for (String s : afterSplit) {
+            if (longestWordInSentence < s.length()) {
+                longestWordInSentence = s.length();
+            }
             sList.add(s);
         }
 
         sList.add("*");
-        
+
+        correctLinesWithStars(sList);
+
         return sList;
+    }
+
+    @Override
+    public void correctLinesWithStars(List<String> listToCorrect) {
+        char[] starsList = new char[longestWordInSentence + 4];
+        Arrays.fill(starsList, '*');
+        listToCorrect.set(0, starsList.toString());
+        listToCorrect.set(listToCorrect.size() - 1, String.valueOf(starsList));
     }
 }
