2e329eb6162fdac754964e7dd1ed920f64e7762d
Robert <rszczygielski@luxoft.com>
2016-01-07 12:19:32 +0100

    Code refactoring - create new 2 interface and 2 new classes to improve code readability

---
+++ src/main/java/org/testgwt/client/gui/SomeTextToShow.java
@@ -0,0 +1,45 @@
+package org.testgwt.client.gui;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.*;
+import org.testgwt.client.services.TextManipulatorImpl;
+
+import java.util.List;
+
+/**
+ * Created by RSzczygielski on 2016-01-04.
+ */
+public class SomeTextToShow extends Composite{
+    private VerticalPanel vPanelMain = new VerticalPanel();
+    private FlexTable flexTable = new FlexTable();
+    private TextPosition textPositionRadioButtons = new TextPositionSimplyImpl();
+    private IntertText intertText;
+
+    private TextManipulatorImpl textManipulator;
+
+    public SomeTextToShow(TextManipulatorImpl textManipulator) {
+        this.textManipulator = textManipulator;
+        initWidget(vPanelMain);
+        intertText = new InsertTextSimplyImpl(this);
+        VerticalPanel radioButtons = textPositionRadioButtons.getPanelWithButtons();
+        VerticalPanel vPanelForTextBoxAndButton = intertText.getTextBoxAndButton();
+
+        vPanelMain.add(vPanelForTextBoxAndButton);
+        vPanelMain.add(radioButtons);
+        vPanelMain.add(flexTable);
+    }
+
+    public void updateLabel(List<String> text) {
+        flexTable.removeAllRows();
+
+        for (String s : text) {
+            int row = flexTable.getRowCount();
+            flexTable.setText(row, 0, s);
+        }
+    }
+
+    public void sendTextToManipulate(String text) {
+        textManipulator.afterManipulate(text);
+    }
+}
